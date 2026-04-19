129ec6a57720a56773448bb041ef76bbc251c1b5
pranavgoli9 <pranavgoli9@gmail.com>
2026-04-19 17:58:29 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function HomePage() {
     <>
       <section className="bio">
         <p>
-          I grew up in Hoover, Alabama, and am currently studying Economics and Computer Science at Penn. In my free time, I enjoy watching soccer, learning about different businesses/stocks, playing chess, and reading. 
-          Here you'll find some of my work, things I've been thinking about, and what I'm reading. This is mainly to give myself a place to write my thoughts down, but feel free to look through as well.
+          I grew up in Hoover, Alabama, and am currently studying Economics and Computer Science at Penn. In my free time, I enjoy watching soccer, learning about different businesses & industries, playing chess, and reading. 
+          Here you'll find some of my work, things I've been thinking about, and what I'm reading. This is mainly to give myself a place to write my thoughts down, but feel free to take a look! Pitches coming soon: Sportradar Group (NASDAQ: SRAD), Acadia Healthcare (NASDAQ: ACHC). 
         </p>
       </section>
 
